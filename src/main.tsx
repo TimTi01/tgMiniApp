0b9e01387dabf1388e406f216fc84354ae9f7adf
Tim <TimF1234@yandex.ru>
2024-03-19 16:57:32 +0300

    test MainButton 3

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router/Router.tsx'
 import './index.css'
-import { MainButton, WebAppProvider } from '@vkruglikov/react-telegram-web-app'
+import {WebAppProvider } from '@vkruglikov/react-telegram-web-app'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <WebAppProvider   
     options={{
       smoothButtonsTransition: true,
+      smoothButtonsTransitionMs: 500,
     }}
   >
     <RouterProvider router={router} />
-    <MainButton text="test" />
   </WebAppProvider>
 )

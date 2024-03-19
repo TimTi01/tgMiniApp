a45593e0ef9f4dc85aa78b8b3e9f36ee324a35da
Tim <TimF1234@yandex.ru>
2024-03-19 16:41:32 +0300

    test MainButton

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,17 +1,17 @@
 import { FC } from "react"
 import { MyMap } from "../../Components/MyMap"
 import { useGeoLocation } from "../../hooks/hooks";
-import { BackButton, MainButton, useShowPopup } from "@vkruglikov/react-telegram-web-app";
+import { BackButton, MainButton } from "@vkruglikov/react-telegram-web-app";
 import { Link, Outlet } from "react-router-dom";
 
 export const HomePage: FC = () => {
     const geo = useGeoLocation(); 
     let myGeo = [geo.coordinates?.lat, geo.coordinates?.lng]
-    const showPopup = useShowPopup();
+    // const showPopup = useShowPopup();
 
-    const handleClick = () => {
-        showPopup({message: 'Hello, I am popup'});
-    }
+    // const handleClick = () => {
+    //     showPopup({message: 'Hello, I am popup'});
+    // }
 
     return (
         <>
@@ -24,10 +24,7 @@ export const HomePage: FC = () => {
             <BackButton/>
             <Outlet />
 
-            <MainButton 
-                text="Заправиться тут" 
-                onClick={handleClick} 
-            />
+            <MainButton text="test" />
         </>
     )
 }
